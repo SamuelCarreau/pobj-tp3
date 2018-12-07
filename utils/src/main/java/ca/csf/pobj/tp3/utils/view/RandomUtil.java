f0396e30a56e24ce40560ce438cfa04d292efda0
samuel carreau <samuelcarreau@gmail.com>
2018-12-06 21:49:56 -0500

    RandomUtil static class add

---
+++ utils/src/main/java/ca/csf/pobj/tp3/utils/view/RandomUtil.java
@@ -0,0 +1,14 @@
+package ca.csf.pobj.tp3.utils.view;
+
+import java.util.Random;
+
+public final class RandomUtil {
+
+    private RandomUtil(){
+        // private so it's a static class
+    }
+
+    public static int RandomRange(int min, int max){
+        return (int)(Math.random()*((max-min)+1));
+    }
+}
